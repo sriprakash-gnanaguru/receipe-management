f1227ff6eba03402dcd03bfdc7f8f052dbc038f7
sriprakash-gnanaguru <sriprakashgnanaguru@gmail.com>
2024-03-08 17:27:56 +0100

    initial

---
+++ food-receipe-management/src/main/java/com/food/receipe/manage/foodreceipemanagement/FoodReceipeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.food.receipe.manage.foodreceipemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodReceipeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodReceipeManagementApplication.class, args);
+	}
+
+}
---
+++ food-receipe-management/src/test/java/com/food/receipe/manage/foodreceipemanagement/FoodReceipeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.receipe.manage.foodreceipemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodReceipeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
